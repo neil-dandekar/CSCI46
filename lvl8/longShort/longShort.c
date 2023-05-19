a42943e8049cafc1af4d6ebaa4de3de0670f043e
Neil Dandekar <neildandekar10@gmail.com>
2023-05-18 20:06:12 -0700

    8.2 Now prints number lines added to each file

--- lvl8/longShort/longShort.c
+++ lvl8/longShort/longShort.c
@@ -21,23 +21,14 @@ int main(int argc, char *argv[]) {
     }
 
     char line[1000]; // Line of text read from file
-
+    int numShort = 0;
+    int numLong = 0;
     // Loop through read file
     while(!feof(readf)) {
         fgets(line, 1000, readf); // Get line
 
         int i = 0; // Counter for toupper() and tolower() loops
-        if (strlen(line) >= 20) { // Check length of line
-            // Convert line to uppercase
-            while(line[i]) {
-                line[i] = toupper(line[i]);
-                i++;
-            }
-
-            // Write line to file
-            fputs(line, longf);
-        }
-        else {
+        if (strlen(line) < 20) { // Check length of line
             // Convert line to lowercase
             while(line[i]) {
                 line[i] = tolower(line[i]);
@@ -46,6 +37,18 @@ int main(int argc, char *argv[]) {
             
             // Write line to file
             fputs(line, shortf);
+            numShort++;
+        }
+        else {
+            // Convert line to uppercase
+            while(line[i]) {
+                line[i] = toupper(line[i]);
+                i++;
+            }
+
+            // Write line to file
+            fputs(line, longf);
+            numLong++;
         }
     }
     
@@ -53,6 +56,9 @@ int main(int argc, char *argv[]) {
     fclose(readf);
     fclose(shortf);
     fclose(longf);
+
+    printf("%d lines written to short.txt\n", numShort);
+    printf("%d lines written to long.txt\n", numLong);
 }
 
 // TEST CASES:
