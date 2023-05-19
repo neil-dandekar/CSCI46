b79389227ae4e4e25996a1db9344347c12498840
Neil Dandekar <neildandekar10@gmail.com>
2023-05-19 02:25:12 -0700

    Finished 9.0 Level Up

--- lvl9/makeHash/md5.c
+++ lvl9/makeHash/md5.c
@@ -1,32 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <openssl/md5.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <stdint.h>
+// #include <openssl/md5.h>
 
-char *md5(const char *str, int length) {
-    MD5_CTX c;
-    uint8_t digest[MD5_DIGEST_LENGTH];
-    char *out = (char*) malloc(MD5_DIGEST_LENGTH * 2 + 1);
+// char *md5(const char *str, int length) {
+//     MD5_CTX c;
+//     uint8_t digest[MD5_DIGEST_LENGTH];
+//     char *out = (char*) malloc(MD5_DIGEST_LENGTH * 2 + 1);
 
-    MD5_Init(&c);
+//     MD5_Init(&c);
 
-    while (length > 0) {
-        if (length > 512) {
-            MD5_Update(&c, str, 512);
-        } else {
-            MD5_Update(&c, str, length);
-        }
-        length -= 512;
-        str += 512;
-    }
+//     while (length > 0) {
+//         if (length > 512) {
+//             MD5_Update(&c, str, 512);
+//         } else {
+//             MD5_Update(&c, str, length);
+//         }
+//         length -= 512;
+//         str += 512;
+//     }
 
-    MD5_Final(digest, &c);
+//     MD5_Final(digest, &c);
 
-    for (int n = 0; n < MD5_DIGEST_LENGTH; ++n) {
-        snprintf(out + n*2, 3, "%02x", digest[n]);
-    }
+//     for (int n = 0; n < MD5_DIGEST_LENGTH; ++n) {
+//         snprintf(out + n*2, 3, "%02x", digest[n]);
+//     }
 
-    out[MD5_DIGEST_LENGTH * 2] = '\0';
+//     out[MD5_DIGEST_LENGTH * 2] = '\0';
 
-    return out;
-}
+//     return out;
+// }
